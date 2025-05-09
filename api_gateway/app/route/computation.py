5b0a5ebbbbfc85ee929c76de1560f2178c60738a
Danielle Gatland <danielle.gatland@heigit.org>
2025-05-09 15:46:51 +0200

    fix: resolve computation id for deduplication

--- api_gateway/app/route/computation.py
+++ api_gateway/app/route/computation.py
@@ -29,7 +29,11 @@ async def subscribe_compute_status(websocket: WebSocket, correlation_uuid: UUID)
     'described here: https://docs.celeryq.dev/en/stable/userguide/tasks.html#built-in-states',
 )
 def get_computation_status(correlation_uuid: UUID, request: Request) -> ComputationStateInfo:
-    result = AsyncResult(id=str(correlation_uuid), app=request.app.state.platform.celery_app)
+    computation_uuid = request.app.state.platform.backend_db.resolve_computation_id(
+        correlation_uuid
+    )  # TODO: add tests for all endpoints with correlation_uuid
+
+    result = AsyncResult(id=str(computation_uuid), app=request.app.state.platform.celery_app)
     message = ''
     if type(result.result) in (ClimatoologyUserError, InputValidationError):
         message = str(result.result)
