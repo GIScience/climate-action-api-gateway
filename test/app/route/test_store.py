e906a7b2e73f5acca5a5641dc68269ec4aa9596c
Danielle Gatland <danielle.gatland@heigit.org>
2025-06-06 12:50:53 +0000

    doc: clean up todos and commented out code

--- test/app/route/test_store.py
+++ test/app/route/test_store.py
@@ -5,28 +5,6 @@ def test_fetch_icon(mocked_client):
     assert response.headers['location'] == 'test-presigned-url'
 
 
-# TODO: needs better mocking to complete successfulyy
-
-# def test_fetch_metadata(mocked_client, general_uuid, default_plugin, default_aoi,default_computation_info, default_platform_connection ):
-#     with patch('api_gateway.app.route.plugin.uuid.uuid4', return_value=general_uuid):
-#         _ = mocked_client.post('/plugin/test_plugin', json={'aoi': default_aoi, 'params': {'id':1}})
-#
-#     # Wait for the computation to finish
-#     result = AsyncResult(id=str(general_uuid), app=default_platform_connection.celery_app)
-#     _ = result.get(timeout=5)
-#
-#     response = mocked_client.get(f'/store/{general_uuid}/metadata')
-#
-#     assert response.json() == default_computation_info.model_dump(mode='json')
-
-
-# def test_list_artifacts(mocked_client, general_uuid):
-#     response = mocked_client.get(f'/store/{general_uuid}')
-#
-#     assert response.status_code == 200
-#     assert response.json() == []
-
-
 def test_fetch_artifact(mocked_client, general_uuid):
     response = mocked_client.get(f'/store/{general_uuid}/{general_uuid}', allow_redirects=False)
 
