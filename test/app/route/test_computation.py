e82f0f0e2da898f6372a18d6a632d22c8b87770c
Danielle Gatland <danielle.gatland@heigit.org>
2025-10-17 17:04:23 +0200

    fix(test): for computation state

--- test/app/route/test_computation.py
+++ test/app/route/test_computation.py
@@ -1,7 +1,9 @@
 import time
+import uuid
 from unittest.mock import patch
 
 import pytest
+from celery.result import AsyncResult
 from climatoology.base.event import ComputationState
 from starlette.websockets import WebSocketDisconnect
 
@@ -17,10 +19,20 @@ def test_computation_status_unknown(mocked_client, general_uuid):
     assert response.status_code == 404
 
 
-def test_computation_status_pending(mocked_client, deduplicated_uuid, backend_with_computations):
-    response = mocked_client.get(f'/computation/{deduplicated_uuid}/state')
+def test_computation_status_success(mocked_client, default_plugin, default_aoi_pure_dict):
+    # pytest-celery uses a CacheBackend instead of our DatabaseBackend, so we need to actually send a test for it to
+    # exist to celery and be able to be created as an AsyncResult to correctly query it's state.
+    # TODO: discuss this
+
+    correlation_uuid = uuid.uuid4()
+    with patch('api_gateway.app.route.plugin.uuid.uuid4', return_value=correlation_uuid):
+        response = mocked_client.post('/plugin/test_plugin', json={'aoi': default_aoi_pure_dict, 'params': {'id': 1}})
+        result = AsyncResult(id=str(correlation_uuid), backend=default_plugin.backend)
+        _ = result.get(5)
+
+    response = mocked_client.get(f'/computation/{correlation_uuid}/state')
     assert response.status_code == 200
-    assert response.json() == {'state': ComputationState.PENDING.value, 'message': ''}
+    assert response.json() == {'state': ComputationState.SUCCESS.value, 'message': ''}
 
 
 def test_computation_status_revoked_q_time_exceeded(mocked_client, general_uuid, default_aoi_pure_dict, default_plugin):
